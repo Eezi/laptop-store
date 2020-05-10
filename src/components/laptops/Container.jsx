5998001953ecbd6c63f7dd2ade18864ce96c2954
Eezi <tomi.a.salo@outlook.com>
2020-05-10 13:13:48 +0300

    Onnistuneesti haettu kaikki data jsonista

--- src/components/laptops/Container.jsx
+++ src/components/laptops/Container.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import styles from './Container.module.css';
 import { Laptops } from '..';
+import data from '../../data/data.json';
+
+
 
 const Container = () =>  {
     return(
         <div className={styles.Container}>
             <h1>The Laptop Store</h1>
-            <Laptops />
+            <div className={styles.cards_container}>{data.map((el) => {
+                return(<Laptops
+                  key={el.id}
+                  name={el.productName}
+                  cpu={el.cpu}
+                  image={el.image}
+                  screen={el.screen}
+                  price={el.price}
+                  img={el.image}
+                  />
+                  
+                );
+            })}</div>
+          
         </div>
     );
 }
--- src/components/laptops/Laptops.jsx
+++ src/components/laptops/Laptops.jsx
@@ -1,75 +1,30 @@
 import React from 'react';
 import styles from './Laptops.module.css';
+import data from '../../data/data.json';
+import asus from '../../data/img/asus-zenbook-flip-s.jpg';
+import dell from '../../data/img/dell-xps-13.png';
+import huawei from '../../data/img/huawei-matebook-pro.jpg';
+import macbook from '../../data/img/macbook-pro-15.jpg';
+import samsung from '../../data/img/samsung-notebook-9.jpg';
 
 
-const Laptops = () =>  {
+const Laptops = (props) =>  {
     return(
-        <div className={styles.Laptop}>
-            <div className={styles.cards_container}>
+        <div className={styles.cards_container}>
             <div className={styles.card}>
                 <div className={styles.card_hero}>
-                    <img className={styles.card_img} src={ require('../../data/img/macbook-pro-15.jpg')}></img>
+                    <img className={styles.card_img} src={ require(`../../data/img/${props.img}`) }></img>
                 </div>
-                    <h2 className={styles.card_name}>Apple Macbook Pro 2018</h2>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🖥</span> 15-inch Retina display</p>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🧮</span> 6-core Intel i7, 8th generation</p>
+                    <h2 className={styles.card_name}>{props.name}</h2>
+                    <p className={styles.card_detail}><span className={styles.emoji_left}>🖥</span> {props.screen}</p>
+                    <p className={styles.card_detail}><span className={styles.emoji_left}>🧮</span> {props.cpu}</p>
                 <div className={styles.card_footer}>
-                    <p className={styles.card_price}>$3199</p>
+                    <p className={styles.card_price}>{props.price}€</p>
                     <p className={styles.card_link}>Check it out </p>
                 </div>
-            </div>
-          
-            <div className={styles.card}>
-                <div className={styles.card_hero}>
-                    <img className={styles.card_img} src={ require('../../data/img/huawei-matebook-pro.jpg')}></img>
-                </div>
-                    <h2 className={styles.card_name}>Huawei matebook X pro</h2>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🖥</span> 13.9-inch, 3K(3,000 x 2,2080)</p>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🧮</span> Intel Core i7, 8th generation</p>
-                <div className={styles.card_footer}>
-                    <p className={styles.card_price}>$1449></p>
-                    <p className={styles.card_link}>Check it out </p>
-                </div>
-            </div>
-            <div className={styles.card}>
-                <div className={styles.card_hero}>
-                    <img className={styles.card_img} src={ require('../../data/img/dell-xps-13.png')}></img>
-                </div>
-                    <h2 className={styles.card_name}>Dell XPS 13</h2>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🖥</span> 13-inch Full HD</p>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🧮</span> 6-core Intel i7, 8th generation</p>
-                <div className={styles.card_footer}>
-                    <p className={styles.card_price}>$3199</p>
-                    <p className={styles.card_link}>Check it out </p>
-                </div>
-            </div>
-            <div className={styles.card}>
-                <div className={styles.card_hero}>
-                    <img className={styles.card_img} src={ require('../../data/img/asus-zenbook-flip-s.jpg')}></img>
-                </div>
-                    <h2 className={styles.card_name}>Apple Macbook Pro 2018</h2>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🖥</span> 15-inch Retina display</p>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🧮</span> 6-core Intel i7, 8th generation</p>
-                <div className={styles.card_footer}>
-                    <p className={styles.card_price}>$3199</p>
-                    <p className={styles.card_link}>Check it out </p>
-                </div>
-            </div>
-            <div className={styles.card}>
-                <div className={styles.card_hero}>
-                    <img className={styles.card_img} src={ require('../../data/img/samsung-notebook-9.jpg')}></img>
-                </div>
-                    <h2 className={styles.card_name}>Apple Macbook Pro 2018</h2>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🖥</span> 15-inch Retina display</p>
-                    <p className={styles.card_detail}><span className={styles.emoji_left}>🧮</span> 6-core Intel i7, 8th generation</p>
-                <div className={styles.card_footer}>
-                    <p className={styles.card_price}>$3199</p>
-                    <p className={styles.card_link}>Check it out </p>
-                </div>
-            </div>
-            
+            </div>      
         </div>   
-        </div>
+   
     );
 }
 
